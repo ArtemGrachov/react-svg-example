9afdca57e34a05ab4ac020bddf948dc31bf4ea00
Artem Grachov <grachov2195@gmail.com>
2024-10-07 10:06:17 +0300

    SVG example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+import iconHeartSrc from './assets/icons/heart.svg';
+import { ReactComponent as IconPhoto } from './assets/icons/photo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SVG example</h1>
+      <div>
+        <h2>
+          Img SRC
+        </h2>
+        <img src={iconHeartSrc} alt="" />
+      </div>
+      <div>
+        <h2>
+          React component
+        </h2>
+        <IconPhoto />
+      </div>
     </div>
   );
 }
